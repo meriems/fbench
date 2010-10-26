603aaea37c08976e7044f449b02d56e9c8d1a433
aschwarte10@googlemail.com <aschwarte10@googlemail.com@448d59bb-d1c0-2985-a03c-9d90f50fb865>
2010-10-26 09:54:14 +0000

    ClassPath + new batch

---
+++ trunk/EvalBenchmark/runEval-setup.bat
@@ -0,0 +1,2 @@
+@Echo off
+java -Dlog4j.configuration=file:config\log4j.properties -cp lib\fbench.jar;lib\slf4j\*;lib\log4j\*;lib\sesame\*;lib\commons\*;lib\lucene\* org.semanticweb.fbench.FederationEval -setup %*
