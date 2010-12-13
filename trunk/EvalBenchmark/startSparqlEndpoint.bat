a56197f4a8e925a1100078ad31df94689db9ffcd
aschwarte10@googlemail.com <aschwarte10@googlemail.com@448d59bb-d1c0-2985-a03c-9d90f50fb865>
2010-12-13 14:23:23 +0000

    Executables

--- trunk/EvalBenchmark/startSparqlEndpoint.bat
+++ trunk/EvalBenchmark/startSparqlEndpoint.bat
@@ -1,2 +1,3 @@
 @Echo off
-java -Xmx512m -Dlog4j.configuration=file:config\log4j-sparql.properties -cp lib\fbench.jar;lib\jetty\*;lib\log4j\*;lib\slf4j\*;lib\sesame\* org.semanticweb.fbench.sparqlendpoint.StartJettySparqlEndpoint %*
+java -Xmx1024m -Dlog4j.configuration=file:config\log4j-sparql.properties -cp lib\fbench.jar;lib\jetty\*;lib\log4j\*;lib\slf4j\*;lib\sesame\* org.semanticweb.fbench.sparqlendpoint.StartJettySparqlEndpoint %*
+exit
