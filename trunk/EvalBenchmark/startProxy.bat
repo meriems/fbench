18738845fd8dab376aa6376ab1dffc249da11ff8
aschwarte10 <aschwarte10@448d59bb-d1c0-2985-a03c-9d90f50fb865>
2011-04-26 12:58:05 +0000

    Refactoring

--- trunk/EvalBenchmark/startProxy.bat
+++ trunk/EvalBenchmark/startProxy.bat
@@ -1,2 +1,2 @@
 @Echo off
-java -Dlog4j.configuration=file:config\log4j-proxy.properties -cp lib\fbench.jar;lib\jetty7\*;lib\log4j\* org.semanticweb.fbench.proxy.StartJettyProxy %*
+java -Dlog4j.configuration=file:config\log4j-proxy.properties -cp lib\fedbench.jar;lib\jetty7\*;lib\log4j\* org.semanticweb.fbench.proxy.StartJettyProxy %*
