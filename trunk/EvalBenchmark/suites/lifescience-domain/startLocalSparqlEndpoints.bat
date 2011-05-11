4e0c4607125039374291432e3a8ebbced8d8145b
aschwarte10 <aschwarte10@448d59bb-d1c0-2985-a03c-9d90f50fb865>
2011-05-11 13:38:44 +0000

    Suites Updates

--- trunk/EvalBenchmark/suites/lifescience-domain/startLocalSparqlEndpoints.bat
+++ trunk/EvalBenchmark/suites/lifescience-domain/startLocalSparqlEndpoints.bat
@@ -1,9 +1,11 @@
 @echo off
 cd ..\..
+SET baseDir=C:\Data\repositories
+
 echo Starting local SPARQL endpoints
 
 REM DBPedia351 => 10000
-echo Starting DBPedia Endpoint on port 10000
-start startSparqlEndpoint.bat data\repositories\native-storage.dbpedia351 10000
-
-REM TODO
+start startSparqlEndpoint.bat %baseDir%\native-storage.dbpedia351 10000
+start startSparqlEndpoint.bat %baseDir%\native-storage.KEGG 10001
+start startSparqlEndpoint.bat %baseDir%\native-storage.chEBI 10002
+start startSparqlEndpoint.bat %baseDir%\native-storage.drugbank 10003
