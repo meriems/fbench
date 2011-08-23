0f9068ba4c2cb7cec1afddee6c25234992de729a
olaf.goerlitz <olaf.goerlitz@448d59bb-d1c0-2985-a03c-9d90f50fb865>
2011-08-23 07:57:10 +0000

    updated SPLENDID implementation and evaluation configuration

--- trunk/EvalBenchmark/suites/WeST/runCross.sh
+++ trunk/EvalBenchmark/suites/WeST/runCross.sh
@@ -2,12 +2,12 @@
 
 echo Running the Cross Benchmark
 
-result_dir=suites/WeST/result
+result_dir=suites/SPLENDID/result
 
 cd ../..
 mkdir $result_dir
 
-./runEval.sh suites/WeST/crossDomain-config.prop
-mv result/loadTimes.csv $result_dir/WeST-cross-load.csv
-mv result/result.csv $result_dir/WeST-cross-result.csv
-mv result/result.nt $result_dir/WeST-cross-result.nt
+./runEval.sh suites/SPLENDID/crossDomain-config.prop
+mv result/loadTimes.csv $result_dir/SPLENDID-cross-load.csv
+mv result/result.csv $result_dir/SPLENDID-cross-result.csv
+mv result/result.nt $result_dir/SPLENDID-cross-result.nt
--- trunk/EvalBenchmark/suites/WeST/runLifeScience.bat
+++ trunk/EvalBenchmark/suites/WeST/runLifeScience.bat
@@ -2,9 +2,9 @@
 cd ..\..
 echo Running the Life Science Benchmark
 
-mkdir suites\olaf\result
+mkdir suites\SPLENDID\result
 
-call runEval.bat suites\olaf\lifeScience-config.prop
-move result\loadTimes.csv suites\olaf\result\olaf-lifeScience-load.csv
-move result\result.csv suites\olaf\result\olaf-lifeScience-result.csv
-move result\result.nt suites\olaf\result\olaf-lifeScience-result.nt
+call runEval.bat suites\SPLENDID\lifeScience-config.prop
+move result\loadTimes.csv suites\SPLENDID\result\SPLENDID-lifeScience-load.csv
+move result\result.csv suites\SPLENDID\result\SPLENDID-lifeScience-result.csv
+move result\result.nt suites\SPLENDID\result\SPLENDID-lifeScience-result.nt
